6531ae91c72359b54169700403efb3a1b520cb11
Gurkirat <sainigurkirat111@gmail.com>
2021-05-09 15:04:45 -0500

    1.1 **report any bugs**

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,3 @@
 module.exports = () => {
-    console.log('Advanced Command and Event handler template by Oct.\nDiscord: Oct#8995\nWebsite: https://octbot.ml\nGitHub: Octille')
-    
+    console.log('Advanced Command and Event handler template by Oct.\nDiscord: Oct#8995\nWebsite: https://octbot.ml\nGitHub: https://github.com/Octille/Discord-Bot-Template')
 }
